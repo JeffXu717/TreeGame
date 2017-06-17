8ab2ba51c177016ec502708a597db72eac965504
hx14384 <hx14384@my.bristol.ac.uk>
2017-06-18 04:30:05 +0800

    BreatheEffect

--- Assets/Scripts/ShowEffect/BreatheEffect.cs
+++ Assets/Scripts/ShowEffect/BreatheEffect.cs
@@ -1,30 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace WTF.TreeGame.EffectComponent
 {
     public class BreatheEffect : GeneralShowEffect{
 
-        private Animator _animator;
+        //private Animator _animator;
+        private Sequence s; 
+
+        public void Start()
+        {
+            StartEffect();
+        }
 
         public override void StartEffect()
         {
-            _animator = GetComponent<Animator>();
-            Debug.Assert(_animator != null, "Error: BreatheEffect lacks of Animator..");
-            _animator.SetTrigger("Start");
+            //_animator = GetComponent<Animator>();
+            //Debug.Assert(_animator != null, "Error: BreatheEffect lacks of Animator..");
+            //_animator.SetTrigger("Start");
+            if (s == null)
+            {
+                s = DOTween.Sequence();
+                Vector3 tempScale = transform.localScale;
+                s.Append(transform.DOScale(tempScale * 1.05f, 3.3f).SetEase(Ease.OutQuart));
+                s.Append(transform.DOScale(tempScale, 2.7f));
+                s.SetLoops(-1);
+            }
+            else
+            {
+                s.Restart(); 
+            }
+
+
         }
 
         public override void PauseEffect()
         {
-            Debug.Assert(_animator != null, "Error: BreatheEffect lacks of Animator..");
-            _animator.SetTrigger("Pause");
+            //Debug.Assert(_animator != null, "Error: BreatheEffect lacks of Animator..");
+            //_animator.SetTrigger("Pause");
+            if (s != null && s.IsPlaying())
+                s.Pause();
         }
 
         public override void StopEffect()
         {
-            Debug.Assert(_animator != null, "Error: BreatheEffect lacks of Animator..");
-            _animator.SetTrigger("Stop");
+            //Debug.Assert(_animator != null, "Error: BreatheEffect lacks of Animator..");
+            //_animator.SetTrigger("Stop");
+            if (s != null && s.IsPlaying())
+                s.Kill();
         }
     }
 }
